899f78ad8c22acdec6442cbf4a55b7af794598fb
meutband <markevansatx@gmail.com>
2020-10-30 13:00:42 -0500

    Updates

---
+++ precache-manifest.fdfaf04a15223be105f56944139b4711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3429ad64a86c3a90c297564079d671bf",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "eebb82d90b9dd917d842",
+    "url": "/burger-app/static/css/main.01a3f8e7.chunk.css"
+  },
+  {
+    "revision": "462f4c33db9304e9d5c6",
+    "url": "/burger-app/static/js/2.73b74e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-app/static/js/2.73b74e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebb82d90b9dd917d842",
+    "url": "/burger-app/static/js/main.aeedda49.chunk.js"
+  },
+  {
+    "revision": "6f7cba2a4194cfdae33f",
+    "url": "/burger-app/static/js/runtime-main.1f272ab5.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
